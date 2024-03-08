f4cb08be5490f297807a84cc3b71c2c06e966929
Anggerr <anggersimbatan@gmail.com>
2024-03-08 20:54:34 +0700

    init block

--- app/src/main/java/com/example/aplikasikotlin/example/myapp/Aquarium.kt
+++ app/src/main/java/com/example/aplikasikotlin/example/myapp/Aquarium.kt
@@ -11,4 +11,12 @@ class Aquarium(length: Int = 100, width: Int = 20, height: Int = 40) {
                 "Length: $length cm " +
                 "Height: $height cm ")
     }
+
+    init {
+        println("aquarium initializing")
+    }
+    init {
+        // 1 liter = 1000 cm^3
+        println("Volume: ${width * length * height / 1000} liters")
+    }
 }
